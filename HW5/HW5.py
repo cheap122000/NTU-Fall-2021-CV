d56804c2f564a4db2608548b3f5fb3ee10fed679
Kai.Chen <kaitechstudio@gmail.com>
2021-12-01 00:25:01 +0800

    HW8

--- HW5/HW5.py
+++ HW5/HW5.py
@@ -5,34 +5,40 @@ def dilationImg(img, kernel):
     h, w = img.shape
     ret = np.zeros((h, w), np.uint8)
     kernel_h, kernel_w = len(kernel), len(kernel[0])
-    for i in range(h - kernel_h):
-        for j in range(w - kernel_w):
+    for i in range(0 - int(kernel_h/2), h - int(kernel_h/2)):
+        for j in range(0 - int(kernel_w/2), w - int(kernel_w/2)):
             localMax = 0
             for m in range(kernel_h):
                 for n in range(kernel_w):
+                    if i+m < 0 or j+n < 0 or i+m >= h or j+n >= w: continue
                     if kernel[m][n] == 1:
                         localMax = max(localMax, img[i+m][j+n])
-            for m in range(kernel_h):
-                for n in range(kernel_w):
-                    ret[i+m][j+n] = localMax
+            
+            ret[i+int(kernel_h/2)][j+int(kernel_w/2)] = localMax
     return ret
 
 def erosionImg(img, kernel):
     h, w = img.shape
     ret = np.zeros((h, w), np.uint8)
     kernel_h, kernel_w = len(kernel), len(kernel[0])
-    for i in range(h - kernel_h):
-        for j in range(w - kernel_w):
+    for i in range(0 - int(kernel_h/2), h - int(kernel_h/2)):
+        for j in range(0 - int(kernel_w/2), w - int(kernel_w/2)):
+            validate = True
             localMin = 255
             for m in range(kernel_h):
                 for n in range(kernel_w):
+                    if i+m < 0 or j+n < 0 or i+m >= h or j+n >= w: continue
                     if kernel[m][n] == 1 and img[i+m][j+n] > 0:
-                        localMin = min(localMin, img[i+m][j+n])
+                        if img[i+m][j+n] > 0:
+                            localMin = min(localMin, img[i+m][j+n])
                     elif kernel[m][n] == 1 and img[i+m][j+n] == 0:
-                        continue
-            for m in range(kernel_h):
-                for n in range(kernel_w):
-                    ret[i+m][j+n] = localMin
+                        validate = False
+                        break
+                if not validate:
+                    break
+
+            if validate:
+                ret[i+int(kernel_h/2)][j+int(kernel_w/2)] = localMin
     return ret
 
 def openingImg(img, kernel):
